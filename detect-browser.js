0d860c8edfefe467fcf489e5965c4d5c465944e2
Josef Vacek <vacekj@outlook.com>
2018-03-09 21:35:54 +0100

    Add test page with pretty print output

--- detect-browser.js
+++ detect-browser.js
@@ -31,6 +31,10 @@ function isMobile() {
 }
 
 function getBrowserName() {
+	if (window.chrome) {
+		return 'Chrome';
+	}
+
 	// Opera 8.0+
 	if ((window.opr && window.opr.addons)
 		|| window.opera
@@ -51,17 +55,13 @@ function getBrowserName() {
 	}
 
 	// Internet Explorer 6-11
-	if (/* @cc_on!@*/false || !!document.DOCUMENT_NODE) {
+	if (/* @cc_on!@*/false || document.DOCUMENT_NODE) {
 		return 'Internet Explorer';
 	}
 
 	// Edge 20+
 	if (window.styleMedia) {
 		return 'Microsoft Edge';
-	}
-
-	if (window.chrome && window.chrome.webstore) {
-		return 'Chrome';
 	}
 }
 
