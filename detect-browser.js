582b9560e35fbbf0758c941cae0e221ef7499575
Ahmad Raza <Ahmdrza@users.noreply.github.com>
2018-03-10 02:06:45 +0500

    Revert "Refactor"

--- detect-browser.js
+++ detect-browser.js
@@ -1,5 +1,3 @@
-// @ts-nocheck
-'use strict';
 // detect-browser.js v0.1
 // Get Browser Data
 
@@ -24,146 +22,193 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+ 
 
-
-function isMobile() {
-	return /Mobi/.test(navigator.userAgent);
-}
-
-function getBrowserName() {
-	if (window.chrome) {
-		return 'Chrome';
-	}
+var browser_data = [];
+var languages = [];
+var user_agent;
+var browser;
+var os;
+var os_type;
+var os_bit;
+var device;
+var device_type;
+var timezone;
+var language;
+var screen_resolution;
+var online;
+var cookie_enabled;
+var referer;
+var IP;
+function getBrowser() {
+	"use strict";
+	referer = (document.referer == undefined) ? 'N/A' : document.referer;
+	device = (/Mobi/.test(navigator.userAgent) == true) ? "Mobile" : "Desktop";
 
 	// Opera 8.0+
-	if ((window.opr && window.opr.addons)
-		|| window.opera
-		|| navigator.userAgent.indexOf(' OPR/') >= 0) {
-		return 'Opera';
-	}
+    var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
 
-	// Firefox 1.0+
-	if (typeof InstallTrigger !== 'undefined') {
-		return 'Firefox';
-	}
+    // Firefox 1.0+
+    var isFirefox = typeof InstallTrigger !== 'undefined';
 
-	// Safari 3.0+ "[object HTMLElementConstructor]"
-	if (/constructor/i.test(window.HTMLElement) || (function (p) {
-		return p.toString() === '[object SafariRemoteNotification]';
-	})(!window['safari'])) {
-		return 'Safari';
-	}
+    // Safari 3.0+ "[object HTMLElementConstructor]" 
+    var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
 
-	// Internet Explorer 6-11
-	if (/* @cc_on!@*/false || document.DOCUMENT_NODE) {
-		return 'Internet Explorer';
-	}
+    // Internet Explorer 6-11
+    var isIE = /*@cc_on!@*/false || !!document.documentMode;
 
-	// Edge 20+
-	if (window.styleMedia) {
-		return 'Microsoft Edge';
-	}
-}
+    // Edge 20+
+    var isEdge = !isIE && !!window.StyleMedia;
 
-function getOSName() {
-	var os;
-	if (isMobile()) {
-		if (/Windows/.test(navigator.userAgent)) {
-			os = 'Windows';
-			if (/Phone 8.0/.test(navigator.userAgent)) {
-				os += ' Phone 8.0';
-			} else if (/Phone 10.0/.test(navigator.userAgent)) {
-				os += ' Phone 10.0';
+    // Chrome 1+
+    var isChrome = !!window.chrome && !!window.chrome.webstore;
+
+    if(isOpera == true)
+    {
+    	browser = "Opera";
+    }
+    else if(isFirefox == true)
+    {
+    	browser = "FireFox";
+    }
+    else if(isSafari == true)
+    {
+    	browser = "Safari";
+    }
+    else if(isIE == true)
+    {
+    	browser = "Internet Explorer";
+    }
+    else if(isEdge == true)
+    {
+    	browser = "Microsoft Edge";
+    }
+    else if(isChrome == true)
+    {
+    	browser = "Chrome";
+    }
+
+	if(device == "Desktop")
+	{
+		if(/Windows/.test(navigator.userAgent) == true){
+			os = "Windows";
+			if(/5.1;/.test(navigator.userAgent) == true)
+			{
+				os = os + " XP";
 			}
-		} else if (/Android/.test(navigator.userAgent)) {
-			function androidVersion() {
-				if (/Android/.test(navigator.appVersion)) {
-					var v = (navigator.appVersion).match(/Android (\d+).(\d+)/);
-					return v;
-				}
+			else if(/6.0;/.test(navigator.userAgent) == true)
+			{
+				os = os + " Vista";
+			}
+			else if(/6.1;/.test(navigator.userAgent) == true)
+			{
+				os = os + " 7";
+			}
+			else if(/6.2/.test(navigator.userAgent) == true)
+			{
+				os = os + " 8";
+			}
+			else if(/10.0;/.test(navigator.userAgent) == true)
+			{
+				os = os + " 10";
 			}
 
-			var ver = androidVersion();
+			if(/64/.test(navigator.userAgent) == true)
+			{
+				os = os + " 64-bit";
+			}
+			else
+			{
+				os = os +" 32-bit";
+			}
+		}
+		else if(/Macintosh/.test(navigator.userAgent) == true)
+		{
+			os = "Macintosh";
+			if(/OS X/.test(navigator.userAgent) == true)
+			{
+				os = os + ' OS X';
+			}
+		}
+	}
+	else if(device == "Mobile")
+	{
+		if(/Windows/.test(navigator.userAgent) == true){
+			os = "Windows";
+			if(/Phone 8.0/.test(navigator.userAgent) == true)
+			{
+				os = os + " Phone 8.0";
+			}
+			else if(/Phone 10.0/.test(navigator.userAgent) == true)
+			{
+				os = os + " Phone 10.0";
+			}
+		}
+		else if(/Android/.test(navigator.userAgent) == true){
+			function AndroidVersion() {
+			    if (/Android/.test(navigator.appVersion)) {
+			        var v = (navigator.appVersion).match(/Android (\d+).(\d+)/);
+			        return v;
+			    }
+			}
+
+			var ver = AndroidVersion();
 			os = ver[0];
-		} else if (/iPhone;/.test(navigator.userAgent)) {
-			function iOSversion() {
-				if (/iP(hone|od|ad)/.test(navigator.appVersion)) {
-					var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
-					return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
-				}
-			}
-
-			var ver = iOSversion();
-			os = 'iOS ' + ver[0] + '.' + ver[1] + '.' + ver[2];
-		} else if (/iPad;/.test(navigator.userAgent)) {
-			function iOSversion() {
-				if (/iP(hone|od|ad)/.test(navigator.appVersion)) {
-					var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
-					return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
-				}
-			}
-
-			var ver = iOSversion();
-			os = 'iOS ' + ver[0] + '.' + ver[1] + '.' + ver[2];
-		} else if (/BBd*/.test(navigator.userAgent)) {
-			os = 'BlackBerry';
 		}
-	} else {
-		if (/Windows/.test(navigator.userAgent)) {
-			os = 'Windows';
-			if (/5.1;/.test(navigator.userAgent)) {
-				os += ' XP';
-			} else if (/6.0;/.test(navigator.userAgent)) {
-				os += ' Vista';
-			} else if (/6.1;/.test(navigator.userAgent)) {
-				os += ' 7';
-			} else if (/6.2/.test(navigator.userAgent)) {
-				os += ' 8';
-			} else if (/10.0;/.test(navigator.userAgent)) {
-				os += ' 10';
+		else if(/iPhone;/.test(navigator.userAgent) == true){
+			function iOSversion() {
+			    if (/iP(hone|od|ad)/.test(navigator.appVersion)) {
+			        var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+			        return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+			    }
 			}
 
-			if (/64/.test(navigator.userAgent)) {
-				os += ' 64-bit';
-			} else {
-				os += ' 32-bit';
+			var ver = iOSversion();
+			os = "iOS "+ver[0]+"."+ver[1]+"."+ver[2];
+		}
+		else if(/iPad;/.test(navigator.userAgent) == true){
+			function iOSversion() {
+			    if (/iP(hone|od|ad)/.test(navigator.appVersion)) {
+			        var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+			        return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+			    }
 			}
-		} else if (/Macintosh/.test(navigator.userAgent)) {
-			os = 'Macintosh';
-			if (/OS X/.test(navigator.userAgent)) {
-				os += ' OS X';
-			}
+
+			var ver = iOSversion();
+			os = "iOS "+ver[0]+"."+ver[1]+"."+ver[2];
+		}
+		else if(/BBd*/.test(navigator.userAgent) == true){
+			os = "BlackBerry";
 		}
 	}
 
-	return os;
-}
 
-function getBrowser() {
-	return {
-		os: getOSName(),
-		browser: getBrowserName(),
+	
+	browser_data = {
 		language: navigator.language,
 		languages: navigator.languages,
 		user_agent: navigator.userAgent,
-		device: isMobile() ? 'Mobile' : 'Desktop',
-		referrer: document.referrer || 'N/A',
+		browser: browser,
+		device: device,
+		referer: referer,
+		os: os,
 		online: navigator.onLine,
 		timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-		screen_resolution: screen.width + ' x ' + screen.height,
-		cookie_enabled: navigator.cookieEnabled,
-	};
+		screen_resolution: screen.width+' x '+screen.height,
+		cookie_enabled: navigator.cookieEnabled
+	}
+	return browser_data;
 }
 
 
-// var xhr = new XMLHttpRequest();
-// xhr.open('GET', 'http://ip-api.com/json');
-// xhr.onreadystatechange = function () {
-// 	if (xhr.readyState == 4) {
-// 		if (xhr.status == 200) {
-// 			var IPdata = xhr.responseText;
-// 			jsonResponse = JSON.parse(IPdata);
-// 		}
-// 	}
-// };
-// xhr.send(null);
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://ip-api.com/json');
+xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+            var IPdata = xhr.responseText;
+            jsonResponse = JSON.parse(IPdata);
+        }
+    }
+};
+xhr.send(null);
